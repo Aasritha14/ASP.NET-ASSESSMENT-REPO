178bfb2c05e4c33d972e37ca1564a1778312f0b3
Aasritha14 <Aasritha.Ramineni@infinite.com>
2025-12-29 12:32:55 +0530

    Add files via upload

---
+++ ValidatorProducts.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ValidatorProducts.aspx.cs" Inherits="Assignment_2.ValidatorProducts" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>DropDown List</h1>
+
+            Select Product:
+            <asp:DropDownList ID="DDLproducts" runat="server" AutoPostBack="true"
+            OnSelectedIndexChanged="DDLproducts_SelectedIndexChanged">
+            </asp:DropDownList>
+<br /><br />
+ 
+           <asp:Image ID="ImageProduct" runat="server" Width="300" />
+<br /><br />
+ 
+           <asp:Button ID="btnPrice" runat="server" Text="Get Price"
+            OnClick="btnPrice_Click" />
+<br /><br />
+ 
+           <asp:Label ID="lblPrice" runat="server" Font-Bold="true" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ValidatorProducts.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_2
+{
+    public partial class ValidatorProducts : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DDLproducts.Items.Add("Laptop");
+                DDLproducts.Items.Add("Iphone");
+                DDLproducts.Items.Add("FaceWash");
+            }
+        }
+
+        protected void DDLproducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DDLproducts.SelectedValue == "Laptop")
+                ImageProduct.ImageUrl = "https://tse2.mm.bing.net/th/id/OIP.Icxy0CXMf8q-atjlr5p4VgHaFj?w=221&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7";
+            else if (DDLproducts.SelectedValue == "Iphone")
+                ImageProduct.ImageUrl = "https://tse4.mm.bing.net/th/id/OIP.bZ9VL25C1aIQNswoo8SqyQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3";
+
+            else if (DDLproducts.SelectedValue == "FaceWash")
+                ImageProduct.ImageUrl = "https://tse1.mm.bing.net/th/id/OIP.xAKbsms8KG3StZQDs3yD_gHaEO?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3";
+
+        }
+
+        protected void btnPrice_Click(object sender, EventArgs e)
+        {
+            if (DDLproducts.SelectedValue == "Laptop")
+                lblPrice.Text = "Price: ₹80,000";
+
+            else if (DDLproducts.SelectedValue == "Iphone")
+                lblPrice.Text = "Price : ₹1,20,000";
+
+            else if (DDLproducts.SelectedValue == "FaceWash")
+                lblPrice.Text = "Price : ₹200";
+        }
+    }
+}
