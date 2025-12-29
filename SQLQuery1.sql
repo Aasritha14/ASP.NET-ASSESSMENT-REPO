1fec26d9ede72a5814570c861cff6f2d59f96f0b
Aasritha14 <Aasritha.Ramineni@infinite.com>
2025-12-29 12:30:35 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+Create Database ElectricityBillDB;
+
+
+Create Table ElectricityBill 
+(
+consumer_number Varchar(20),
+consumer_name Varchar(50),
+units_consumed Int,
+bill_amount Float
+);
+
+select * from ElectricityBill
